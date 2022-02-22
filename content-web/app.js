db79f35c6842afe5de4dc5b9860978c4dc2bdf0d
guidickie <gui@dickie.com.br>
2022-02-22 23:53:52 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("584a5f98-0a89-4aca-9dc5-3ac8728a2b12");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
